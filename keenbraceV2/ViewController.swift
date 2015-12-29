c8eb0d8eee539989dfb65114eaace321a208300f
michaeltam <miketam@michaeltamdemacbook-pro.local>
2015-12-29 15:25:26 +0800

    Initial Commit

---
+++ keenbraceV2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  keenbraceV2
+//
+//  Created by michaeltam on 15/12/29.
+//  Copyright © 2015年 michaeltam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
